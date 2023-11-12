bde43129dcd45eacb5d8d621e8ae864e0ed07f63
Nick Dalalelis <ndalalelis@gmail.com>
2023-11-12 08:26:10 -0500

    Chat view

--- samples/piggy-bank-openai-app/frontend/views/chat/ChatView.tsx
+++ samples/piggy-bank-openai-app/frontend/views/chat/ChatView.tsx
@@ -1,30 +1,32 @@
 import { Button } from '@hilla/react-components/Button.js';
+//https://hilla.dev/docs/react/components/message-list
+import {MessageList, MessageListItem} from "@hilla/react-components/MessageList";
+//https://hilla.dev/docs/react/components/message-input
+import { MessageInput } from '@hilla/react-components/MessageInput';
 import { Notification } from '@hilla/react-components/Notification.js';
 import { TextField } from '@hilla/react-components/TextField.js';
+import { sayHello } from 'Frontend/generated/HelloWorldService';
 import { HelloWorldService } from 'Frontend/generated/endpoints.js';
 import { useState } from 'react';
 
 export default function ChatView() {
-    const [name, setName] = useState('');
+    const [messages, setMessages] = useState<MessageListItem[]>([]);
+
+    function addMessage(message: MessageListItem) {
+        setMessages([...messages, message]);
+    }
+
+    async function sayHello(name: string) {
+        addMessage({ userName: 'You', text: name });
+
+        const message = await HelloWorldService.sayHello(name);
+        addMessage({ userName: 'Bot', text: message });
+    }
 
     return (
-      <>
-        <section className="flex p-m gap-m items-end">
-          <TextField
-            label="Your name"
-            onValueChanged={(e) => {
-              setName(e.detail.value);
-            }}
-          />
-          <Button
-            onClick={async () => {
-              const serverResponse = await HelloWorldService.sayHello(name);
-              Notification.show(serverResponse);
-            }}
-          >
-            Say hello
-          </Button>
-        </section>
-      </>
+      <div className="p-m flex flex-col h-full box-border">
+        <MessageList items={messages} />
+        <MessageInput onSubmit={e => sayHello(e.detail.value)} />
+      </div>
     );
 }
